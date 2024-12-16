704fe38f4cf1eb9fad46f00999e0f97caea22e51
Phuoc Dat <deltora1st@gmail.com>
2024-12-16 17:27:22 +0700

    Change project VueJS By LE PHUOC DAT - at 20241216 Change:  - Template syntax.

--- vue-project/src/components/TestItem.vue
+++ vue-project/src/components/TestItem.vue
@@ -1,9 +1,20 @@
 <script setup>
+let rawHtml = '<span style="color: red">This should be red.</span>';
 </script>
 
 <template>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
   <h1>Wanna test?</h1>
 </template>
 
 <style scoped>
 </style>
+
+<!-- 
+- Shorthand: 
+1) v-bind ~ :
+2) v-on ~ @
+- Dynamic Arguments: <a v-bind:[attributeName]="url"> ... </a> (Wrapping it with square brackets)
+3) Modifiers:
+-->
