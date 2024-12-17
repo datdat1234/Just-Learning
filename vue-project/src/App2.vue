fe7498eedafaac115faf04d528619d6e47c7ca25
Phuoc Dat <deltora1st@gmail.com>
2024-12-17 21:31:21 +0700

    Change project VueJS By LE PHUOC DAT - at 20241217 Change:  - Reactivity Fundamentals.

--- vue-project/src/App2.vue
+++ vue-project/src/App2.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TestItem from './components/TestItem.vue'
+import TestItem1 from './components/TestItem1.vue'
 </script>
 
 <template>
   <TestItem/>
+  <TestItem1/>
 </template>
 
 <style scoped>
