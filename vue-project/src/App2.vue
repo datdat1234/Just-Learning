cba6cc93fae76d1138bde85cb09e05dabfcce223
Phuoc Dat <deltora1st@gmail.com>
2024-12-19 13:46:42 +0700

    Change of project VueJS By LE PHUOC DAT - at 20241218 Change: - Class and Style Bindings.

--- vue-project/src/App2.vue
+++ vue-project/src/App2.vue
@@ -1,13 +1,9 @@
 <script setup>
-import TestItem from './components/TestItem.vue'
-import TestItem1 from './components/TestItem1.vue'
-import TestItem2 from './components/TestItem2.vue'
+import TestItem3 from './components/TestItem3.vue'
 </script>
 
 <template>
-  <TestItem/>
-  <TestItem1/>
-  <TestItem2/>
+  <TestItem3/>
 </template>
 
 <style scoped>
