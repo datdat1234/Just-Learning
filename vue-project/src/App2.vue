f4731541a66b098e72065216a1209777bb434da1
Phuoc Dat <deltora1st@gmail.com>
2024-12-23 15:34:13 +0700

    Change of project VueJS By LE PHUOC DAT - at 20241223 Change: - Unique key and List Rendering.

--- vue-project/src/App2.vue
+++ vue-project/src/App2.vue
@@ -1,10 +1,19 @@
 <script setup>
-import TestItem4 from './components/TestItem4.vue'
+import TestItem6 from './components/TestItem6.vue'
 </script>
 
 <template>
-  <TestItem4/>
+  <div class="container"><TestItem6 /></div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f0f0f0;
+}
 </style>
