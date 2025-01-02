7daeb958211c994d2bd404514ccd5d9020d21aa0
LE PHUOC DAT <deltora1st@gmail.com>
2025-01-02 23:52:53 +0700

    By LE PHUOC DAT - at 20250102 Change:  - Chapter 1 -> To page 45 / 7.

---
+++ Java/source/Test7.java
@@ -0,0 +1,8 @@
+public class Test7 {
+private String brand;
+private boolean empty;
+public static void main(String[] args) {
+  Test7 wb = new Test7();
+System.out.print("Empty = " + wb.empty);
+System.out.print(", Brand = " + wb.brand);
+} }
