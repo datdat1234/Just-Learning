19290cf7a1ee423fbdcc5d14d1ca81c3290f5b04
LE PHUOC DAT <deltora1st@gmail.com>
2025-01-09 23:45:13 +0700

    By LE PHUOC DAT - at 20250109 Change:  - Chapter 2 -> To page 72.

---
+++ Java/source/Test9.java
@@ -0,0 +1,7 @@
+public class Test9 {
+  public static void main(String[] args) {
+    int y = 20;
+    System.out.println((y > 5) ? 21 : "Zebra");
+    int animal = (y < 91) ? 9 : "Horse"; // DOES NOT COMPILE
+  } 
+}
