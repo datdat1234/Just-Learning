a8e7ea60dfedfdea5087eb41a678599f9963ccb6
LE PHUOC DAT <deltora1st@gmail.com>
2025-01-19 15:54:44 +0700

    By LE PHUOC DAT - at 20250119 Change:  - Chapter 3 -> To page 129.

---
+++ Java/source/Test11.java
@@ -0,0 +1,14 @@
+public class Test11 {
+  public static void main(String[] args) {
+    // StringBuilder sb = new StringBuilder("animals");
+    // sb.insert(8, "-");
+    // System.out.println(sb);
+
+    String x = "Hello World";
+    String y = "Hello World";
+    System.out.println(x == y);
+
+    String[] x1 = {"HALLO", "WORLD"};
+    System.out.println(java.util.Arrays.toString(x1));
+  } 
+}
