f4dfe07d719cadfccdef5693ea585f063feecb8c
Phuoc Dat <deltora1st@gmail.com>
2025-02-28 09:14:57 +0700

    By LE PHUOC DAT - at 20250228 Change:  - Custom Directives.

--- vue-project/src/App2.vue
+++ vue-project/src/App2.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { useTemplateRef, onMounted } from 'vue'
-import TestItem15 from './components/TestItem15.vue'
+import TestItem16 from './components/TestItem16.vue'
 </script>
 
 <template>
   <div class="container">
-    <component :is="TestItem15"></component>
+    <component :is="TestItem16"></component>
   </div>
 </template>
 
---
+++ vue-project/src/components/TestItem16.vue
@@ -0,0 +1,18 @@
+<script setup>
+// enables v-highlight in templates
+const vHighlight = {
+  mounted: (el) => {
+    el.classList.add('is-highlight')
+  }
+}
+</script>
+
+<template>
+  <p v-highlight>This sentence is important!</p>
+</template>
+
+<style>
+.is-highlight {
+  color: blue;
+}
+</style>
